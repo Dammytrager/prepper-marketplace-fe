c274b3675b1a0124edea227b45b2794683fef7d6
Dammytrager <malomo.damilare@yahoo.com>
2019-11-06 19:02:52 +0100

    Create dropdowns on navbar

--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NOTIFICATIONS} from '../../static/dummy/notifications';
 
 @Component({
   selector: 'plm-navbar',
@@ -8,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
   showSearch = false;
   showMobile = false;
+  notifications = NOTIFICATIONS;
 
   constructor() {}
 
   ngOnInit() {}
 
+  notificationIcon(type) {
+    let icon = [];
+    if (type === 'success') {
+      icon = ['fas', 'check'];
+    } else if (type === 'info') {
+      icon = ['fas', 'plus'];
+    } else if (type === 'warning') {
+      icon = ['fas', 'exclamation-triangle'];
+    } else if (type === 'danger') {
+      icon = ['fas', 'times'];
+    }
+
+    return icon;
+  }
 }
